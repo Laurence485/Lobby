bb7ff93f75fdf9c0abd076eb493d7117b6633b39
Laurence Dunn <laurencesrdunn@gmail.com>
2021-01-09 20:36:04 +0000

    add server class

--- server.py
+++ server.py
@@ -11,86 +11,91 @@ PORT = config['PORT']
 BUFFER_SIZE = config['BUFFER_SIZE']
 CONNECTIONS = config['MAX_CONNECTIONS']
 
-current_player_id = 0
-players = {}
-disconnected_player_ids = []
 
+class Server:
+    """The Server object handles inbound and outbound data to
+    and from the server."""
 
-def client(conn: socket, player_id: int) -> None:
-    with conn:
-        conn.send(pickle.dumps(player_id))
-
-        players[player_id] = network_data()
-
-        while True:
-            try:
-                player_attributes = pickle.loads(conn.recv(BUFFER_SIZE))
-            except EOFError:
-                _disconnect_player(player_id)
-                # There are no players playing.
-                # The last player is not yet removed from the server
-                # as no data is received until another player connects.
-                if len(players) == 1:
-                    _delete_disconnected_players()
-                    _reset_players()
-                break
-            else:
-                players[player_id] = player_attributes
-
-                if not player_attributes:
-                    print('Disconnected from server.')
-                    break
-
-                conn.sendall(pickle.dumps(players))
-
-                if disconnected_player_ids:
-                    _delete_disconnected_players()
-
-
-def _disconnect_player(player_id: int) -> None:
-    # Indicate that this player should be deleted locally.
-    players[player_id]['x'] = None
-    disconnected_player_ids.append(player_id)
-    print(
-        f'Connection dropped ({players[player_id]["username"]},'
-        f' Player id: {player_id}).'
-    )
-
-
-def _delete_disconnected_players() -> None:
-    for id_ in disconnected_player_ids:
-        try:
-            del players[id_]
-            print(f'Deleted player with id {id_} from server.')
-        except KeyError:
-            print(
-                f'Could not delete player ({players[id_]["username"]},'
-                f' id: {id_})  from server.'
-            )
-        disconnected_player_ids.remove(id_)
-
-
-def _reset_players() -> None:
-    """Reset global player variables."""
-    if players:
-        players.clear()
-
-    global current_player_id
     current_player_id = 0
 
-    print('All players reset.')
+    def __init__(self) -> None:
+        self.players = {}
+        self.disconnected_player_ids = []
+
+    def client(self, conn: socket, player_id: int) -> None:
+        with conn:
+            conn.send(pickle.dumps(player_id))
+
+            self.players[player_id] = network_data()
+
+            while True:
+                try:
+                    player_attributes = pickle.loads(conn.recv(BUFFER_SIZE))
+                except EOFError:
+                    self._disconnect_player(player_id)
+                    # There are no players playing.
+                    # The last player is not yet removed from the server
+                    # as no data is received until another player connects.
+                    if len(self.players) == 1:
+                        self._delete_disconnected_players()
+                        self._reset_players()
+                    break
+                else:
+                    self.players[player_id] = player_attributes
+
+                    if not player_attributes:
+                        print('Disconnected from server.')
+                        break
+
+                    conn.sendall(pickle.dumps(self.players))
+
+                    if self.disconnected_player_ids:
+                        self._delete_disconnected_players()
+
+    def _disconnect_player(self, player_id: int) -> None:
+        # Indicate that this player should be deleted locally.
+        self.players[player_id]['x'] = None
+        self.disconnected_player_ids.append(player_id)
+        print(
+            f'Connection dropped ({self.players[player_id]["username"]},'
+            f' Player id: {player_id}).'
+        )
+
+    def _delete_disconnected_players(self) -> None:
+        for id_ in self.disconnected_player_ids:
+            try:
+                del self.players[id_]
+                print(f'Deleted player with id {id_} from server.')
+            except KeyError:
+                print(
+                    f'Could not delete player ({self.players[id_]["username"]},'
+                    f' id: {id_})  from server.'
+                )
+            self.disconnected_player_ids.remove(id_)
+
+    def _reset_players(self) -> None:
+        if self.players:
+            self.players.clear()
+
+        self.current_player_id = 0
+
+        print('All players reset.')
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     s.bind((HOST, PORT))
     s.listen(CONNECTIONS)
+    server = Server()
     print('Server started, waiting for connection...')
 
     while True:
         conn, addr = s.accept()
-        print('Connected by:', addr, f'Player id: {current_player_id}')
+        print('Connected by:', addr, f'Player id: {server.current_player_id}')
 
-        threading.Thread(target=client, args=(conn, current_player_id)).start()
+        threading.Thread(
+            target=server.client,
+            args=(conn, server.current_player_id)
+        ).start()
 
-        current_player_id += 1
+        server.current_player_id += 1
