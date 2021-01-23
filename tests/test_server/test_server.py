c3da1f94a02f8cce4422e9a8fc55149e2f568260
Laurence Dunn <laurencesrdunn@gmail.com>
2021-01-23 21:16:00 +0000

    add update player attributes

--- tests/test_server/test_server.py
+++ tests/test_server/test_server.py
@@ -8,22 +8,17 @@ from server.server import Server
 
 @pytest.fixture
 def mock_connection(mock_player):
-    with patch('socket.socket') as mock_socket:
-        mock_socket.recv.return_value = pickle.dumps(mock_player().attributes)
-        # Interrupt infinite loop at end of first iteration.
-        mock_socket.sendall.side_effect = Mock(side_effect=InterruptedError)
-        yield mock_socket
-
-
-@pytest.fixture
-def mock_connection_(mock_player):
-    def _mock_connection_(*args, **kwargs):
+    def _mock_connection(*args, **kwargs):
         with patch('socket.socket') as mock_socket:
             mock_socket.recv.return_value = pickle.dumps(
                 mock_player(*args, **kwargs).attributes
             )
+            # Interrupt infinite loop at end of first iteration.
+            mock_socket.sendall.side_effect = Mock(
+                side_effect=InterruptedError
+            )
             return mock_socket
-    return _mock_connection_
+    return _mock_connection
 
 
 def test_init(mock_os_config):
@@ -37,22 +32,34 @@ def test_init(mock_os_config):
 def test_client_adds_player_attributes(mock_os_config, mock_connection):
     server = Server()
     with pytest.raises(InterruptedError):
-        server.client(mock_connection, 0)
+        server.client(mock_connection(), 0)
 
     with pytest.raises(InterruptedError):
-        server.client(mock_connection, 1)
+        server.client(mock_connection(), 1)
 
     with pytest.raises(InterruptedError):
-        server.client(mock_connection, 2)
+        server.client(mock_connection(), 2)
 
     assert len(server.players) == 3
 
 
-#  patch isnt working here
-def tess_client_updates_player_attributes(mock_os_config, mock_connection_):
+@pytest.mark.parametrize('test_user', [
+    (0, 'TEST_USER_1', 50, 100),
+    (1, 'TEST_USER_2', 123, 456),
+    (2, 'TEST_USER_3', 23, 22)
+    ])
+def test_client_updates_player_attributes(
+    mock_os_config, mock_connection, test_user
+):
     server = Server()
+    player_id, username, x, y = test_user
+
     with pytest.raises(InterruptedError):
-        server.client(mock_connection_(), 0)
+        server.client(mock_connection(*test_user), player_id)
+
+    assert server.players[player_id]['username'] == username
+    assert server.players[player_id]['x'] == x
+    assert server.players[player_id]['y'] == y
 
 
 def test_client_player_disconnected(mock_os_config, mock_connection):
